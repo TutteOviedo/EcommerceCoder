d7b88e365dea4e183f859a108e8ee5f3aa33c261
Matias Oviedo <matiioviedo@gmail.com>
2023-12-13 20:06:22 -0300

    Tercera pre entrega

--- js/index.js
+++ js/index.js
@@ -1,23 +1,26 @@
-
-carrito = [];
-
+// Definir el objeto PRODUCTOS
 const PRODUCTOS = {
     nombre: "",
     categoria: "",
-    precio:"" ,
-    id:"",
-    imagen:"",
-}
+    precio: "",
+    id: "",
+    imagen: "",
+    cantidad: 0,
+};
 
-function Producto(nombre,categoria,precio, id, imagen){
+// Definir la función Producto
+function Producto(nombre, categoria, precio, id, imagen) {
     this.nombre = nombre,
-    this.categoria = categoria,
-    this.precio = precio,
-    this.id = id,
-    this.imagen = imagen
+        this.categoria = categoria,
+        this.precio = precio,
+        this.id = id,
+        this.imagen = imagen
 }
 
-//Cargamos el catalogo
+// Inicializar el carrito
+let carrito = [];
+
+// Definir el catálogo de productos
 const CATALOGO = [
     new Producto("Intel I3", "Microprocesadores", 100, 1, "./assets/I3.jpg"),
     new Producto("Intel I5", "Microprocesadores", 150, 2,"./assets/I5.png"),
@@ -30,15 +33,13 @@ const CATALOGO = [
     new Producto("Gigabyte Ga H310", "Motherboard", 90, 9,"./assets/MOTHERGIGABYTE.png"),
     new Producto("Asus A320m-k", "Motherboard", 80, 10,"./assets/MOTHERASUS.jpg"),
     new Producto("MSI RTX 30 Series 3080ti", "Placa de video", 800, 11,"./assets/GRAFICA.webp"),
-]
+];
 
+// Obtener el contenedor de las tarjetas
 const cardsContainer = document.getElementById('cards');
 
-
-
+// Crear las tarjetas para cada producto en el catálogo
 CATALOGO.forEach(producto => {
-    //Creamos las cards
-
     const card = document.createElement('div');
     card.className = 'card';
 
@@ -63,49 +64,113 @@ CATALOGO.forEach(producto => {
     card.appendChild(cardBody);
 
     const addToCartButton = document.createElement('button');
-    addToCartButton.className = 'btn btn-primary'; 
+    addToCartButton.className = 'btn btn-primary';
     addToCartButton.textContent = 'Agregar al carrito';
-    
-    //Cambiamos de color el boton cuando el mouse pasa por encima y lo reestablecemos cuando lo saca para crea el efecto de hover
+
     addToCartButton.addEventListener('mouseover', () => {
-        addToCartButton.style.backgroundColor = '#87CEEB'; 
-        });
+        addToCartButton.style.backgroundColor = '#87CEEB';
+    });
     addToCartButton.addEventListener('mouseout', () => {
         addToCartButton.style.backgroundColor = '';
-        });
-    
-    //Guardamos el producto en el carrito
+    });
+
     addToCartButton.addEventListener('click', () => {
-        carrito.push({
-            nombre: producto.nombre,
-            precio: producto.precio,
-            imagen: producto.imagen
-            })
-            alert(`Producto ${producto.nombre} agregado al carrito`);
-            console.log("Carrito:", carrito);
+        const productoEnCarrito = carrito.find(item => item.id === producto.id);
+
+        if (productoEnCarrito) {
+            productoEnCarrito.cantidad += 1;
+        } else {
+            carrito.push({
+                id: producto.id,
+                nombre: producto.nombre,
+                precio: producto.precio,
+                imagen: producto.imagen,
+                cantidad: 1,
+            });
         }
-        );
-    
+
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        // Pop up de tostify(Se agrego un producto al carrito)
+        Toastify({
+            text: `El producto "${producto.nombre}" fue agregado al carrito`,
+            duration: 3000,
+            position: 'right',
+            gravity: "bottom",
+            backgroundColor: 'green',
+        }).showToast();
+
+    });
+
     card.appendChild(addToCartButton);
     cardsContainer.appendChild(card);
 });
 
-//Carrito de compras
 
+
+// Obtener el elemento del carrito
 let shoppingCart = document.getElementById('shopping-cart');
-shoppingCart.addEventListener('click', () => {
+
+// Recuperar el carrito almacenado en localStorage al cargar la página
+if (localStorage.getItem('carrito')) {
+    carrito = JSON.parse(localStorage.getItem('carrito'));
+}
+
+// Función para actualizar y mostrar el carrito
+function actualizarYMostrarCarrito() {
     let nombre = prompt("Por favor, ingresa tu nombre:");
     let apellido = prompt("Por favor, ingresa tu apellido:");
     let documento = prompt("Por favor, ingresa tu número de documento:");
-    let facturaCompra = `Datos de facturación:\nNombre: ${nombre}\nApellido: ${apellido}\nDNI: ${documento}\n\nFactura de compra:\n`;
+
+    let facturaCompra = `
+    <div style="text-align: left;">
+        <h2>Datos de facturación:</h2>
+        <p><strong>Nombre:</strong> ${nombre}</p>
+        <p><strong>Apellido:</strong> ${apellido}</p>
+        <p><strong>DNI:</strong> ${documento}</p>
+        <h2>Factura de compra:</h2>
+    `;
+
     let totalCarrito = 0;
 
     carrito.forEach(producto => {
-        facturaCompra += `Producto: ${producto.nombre} | Precio: ${producto.precio} u$d\n`;
-        totalCarrito += producto.precio;
+
+        facturaCompra += `
+            <div style="margin-bottom: 10px;">
+                <img src="${producto.imagen}" alt="${producto.nombre}" style="max-width: 50px; max-height: 50px; margin-right: 10px;">
+                <span><strong>Cantidad:</strong> ${producto.cantidad} x ${producto.nombre} - Precio unitario: ${producto.precio} u$d</span>
+            </div>
+        `;
+
+        // Calcular el precio total del producto
+        totalCarrito += producto.precio * producto.cantidad;
     });
 
-    facturaCompra += `\nTotal: ${totalCarrito} u$d`;
 
-    alert(facturaCompra);
-});
+    facturaCompra += `
+        <div style="margin-top: 20px;">
+            <h3>Total: ${totalCarrito} u$d</h3>
+        </div>
+    </div>
+    `;
+
+    // Utilizar SweetAlert2 para mostrar la factura
+    Swal.fire({
+        title: 'Factura de Compra',
+        html: facturaCompra,
+        icon: 'info', 
+        confirmButtonText: 'Aceptar',
+        width: 'auto', 
+    }).then(() => {
+        // Limpiar el carrito una vez que se imprime la factura
+        carrito = [];
+        localStorage.removeItem('carrito');
+    });
+
+    // Guardar el carrito en localStorage después de actualizarlo
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+shoppingCart.addEventListener('click', actualizarYMostrarCarrito);
+
+
+shoppingCart.addEventListener('click', actualizarYMostrarCarrito);
